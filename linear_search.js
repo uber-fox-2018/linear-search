92f96c0ebf0aa51b5932438ad614fa5d1a4331bc
vinesto <oqvinesto@gmail.com>
2018-07-10 14:01:31 +0700

    first commit

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let  i = 0; i<values.length;i++){
+    if(target === values[i]){
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,15 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  
+  let arrResult = [];
+  for(let i = 0; i< values.length; i++){
+    
+    if(target === values[i]){
+      arrResult.push(i)
+    }
+  }
+  return arrResult
 }
 
 let banana_arr = "banana".split("");
