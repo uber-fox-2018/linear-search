f6e68aa7b61e194f8f37e4bb42bab8523734d3f1
andryjns <andry_jns@yahoo.com>
2018-07-10 19:30:05 +0700

    linear-andry

--- linear_search.js
+++ linear_search.js
@@ -3,8 +3,15 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (var i=0; i<values.length; i++) {
+    if (target === values[i]) {
+        return i
+    }
+    
+  }  
+  return -1
 }
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
@@ -16,7 +23,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var result= []
+  for (var i=0; i<values.length; i++) {
+      if (values[i] === target) {
+          result.push(i)
+      }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
