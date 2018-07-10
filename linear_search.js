3c5b2ebd266dfb3dd0f4a50cdc120f0632050f9c
imamaguss <imam.agussopian@gmail.com>
2018-07-10 20:48:39 +0700

    add linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for(let i = 0; i < values.length; i++) {
+    if(target === values[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +23,15 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let targetPosition = [];
+  for(let i = 0; i < values.length; i++) {
+    if(target === values[i]) {
+      targetPosition.push(i);
+    }
+  }
+
+  return targetPosition;
+
 }
 
 let banana_arr = "banana".split("");
