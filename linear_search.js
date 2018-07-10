342cf1f88659877911f2ba5b8c0fcfd6920b39f3
Your Name <fahrisyany@gmail.com>
2018-07-10 17:25:15 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,20 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let counter = 0
+  for (let i = 0; i < values.length; i++) {
+
+    if (values[i] === target) {
+      counter++
+    }
+  }
+  if (counter === 0) {
+    return -1
+  }
+  return counter
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +28,16 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let arr = []
+  for (let i = 0; i < values.length; i++) {
+
+    if (values[i] === target) {
+      // counter++
+      arr.push(i)
+
+    }
+  }
+  return arr
 }
 
 let banana_arr = "banana".split("");
