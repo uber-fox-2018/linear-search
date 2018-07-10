6fada9a6eb29e95bd865c9cf553234028475d9b4
hacktiv8 <hacktiv8@H8JKT009.localdomain>
2018-07-10 13:52:24 +0700

    first commit

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (var i in values){
+    if (values[i] === target){
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +21,14 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let index = [];
+  for (var i in values){
+    if (values[i] === target){
+      index.push(Number(i))
+    }
+  }
+  if (index.length > 0) return index;
+  return -1;
 }
 
 let banana_arr = "banana".split("");
