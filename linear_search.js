5562a84a91a85222f5b8ddd88f3d8a356c46017e
talkasrul <talkasrul@gmail.com>
2018-07-10 15:49:55 +0700

    Add some code case linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let i = 0; i < values.length; i++) {
+    if(values[i] == target) {
+      return i;
+    }
+  }
+
+  return -1;
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,10 +21,18 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
+
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let hasil = [];
+  for(let i = 0; i < values.length; i++) {
+    if(values[i] == target) {
+      hasil.push(i)
+    }
+  }
+  return hasil;
 }
 
 let banana_arr = "banana".split("");
@@ -28,3 +44,6 @@ module.exports = {
   linearSearch,
   globalLinearSearch
 }
+
+
+console.log("Linear Search")
